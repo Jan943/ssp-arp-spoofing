1ed2988bc053e7c06b169e32c3b592d24c9700be
chohura <56127583+chohura@users.noreply.github.com>
2023-11-15 12:11:07 +0100

    Update topo.py

--- topo.py
+++ topo.py
@@ -3,22 +3,23 @@ class SSPTopo( Topo ):
  "SSP ARP spoofing topology."
  def __init__( self ):
  # Initialize topology
- Topo.__init__( self )
+        Topo.__init__( self )
  # Add hosts and switches
- h1 = self.addHost( 'h1' )
- h2 = self.addHost( 'h2' )
- h3 = self.addHost( 'h3' )
- h4 = self.addHost( 'h4' )
-  
- s1 = self.addSwitch( 's1' ) #linking h1 and h2
- s2 = self.addSwitch( 's2' ) #linking h3 and h4
- s3 = self.addSwitch( 's3' ) #linking s1 and s2
-  
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+
+        s1 = self.addSwitch( 's1' ) #linking h1 and h2
+        s2 = self.addSwitch( 's2' ) #linking h3 and h4
+        s3 = self.addSwitch( 's3' ) #linking s1 and s2
+
  # Add links
- self.addLink( h1, s1 )
- self.addLink( h2, s1 )
- self.addLink( h3, s2 )
- self.addLink( h4, s2 )
- self.addLink( s1, s3 )
- self.addLink( s2, s3 )
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s2 )
+        self.addLink( h4, s2 )
+        self.addLink( s1, s3 )
+        self.addLink( s2, s3 )
 topos = { 'ssptopo': ( lambda: SSPTopo() ) }
+
