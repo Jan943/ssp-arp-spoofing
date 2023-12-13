546da064078e72c1d1552feaf2577578ac37c688
daaalka <56134263+daaalka@users.noreply.github.com>
2023-12-13 11:30:24 +0100

    traffic generator

---
+++ generator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Lista adresów IP hostów
+HOSTS=("10.0.0.1" "10.0.0.2" "10.0.0.3" "10.0.0.4")
+SERWER_PORT=5001
+
+# Pętla wysyłająca dane z każdego hosta do losowo wybranego innego hosta
+for ((i=0; i<${#HOSTS[@]}; ++i)); do
+    # Losowy indeks hosta docelowego
+    index=$((RANDOM % ${#HOSTS[@]}))
+    # Sprawdzenie, czy indeks nie wskazuje na ten sam host
+    while [ $index -eq $i ]; do
+        index=$((RANDOM % ${#HOSTS[@]}))
+    done
+
+    # Losowy port w zakresie od 5000 do 6000
+#    RANDOM_PORT=$((RANDOM % (6000-5000+1) + 5000))
+
+    # Wysłanie danych z hosta o indeksie 'i' do hosta o losowym indeksie 'index' na losowym porcie 'RANDOM_PORT'
+    iperf -c ${HOSTS[$index]} -p $SERWER_PORT -t 3600 -i 5 -P 5 &
+done
+
+# Oczekiwanie na zakończenie wszystkich testów
+wait
